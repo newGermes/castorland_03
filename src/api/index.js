93c3f9dd233b2af02bbd3d947293262f1f2bf0f6
Vitalii <kukhar.vitalii@gmail.com>
2017-12-29 23:17:21 +0200

    reduce code

--- src/api/index.js
+++ src/api/index.js
@@ -3,30 +3,28 @@ import axios from 'axios'
 const _api = 'http://test.nv4hqsgnvx.eu-central-1.elasticbeanstalk.com/'
 const _productParam = 'products?where={"catalogs":"castorland"}&page='
 
-export function getProducts(page) {
-    return new Promise((resolve, reject) => {
-        let isString = toString.call(page) === '[object String]'
-        let query = undefined 
-        let adapter = {
-            items: [],
-            links: {},
-            meta: {}
-        }
+export const getProducts = (page) => new Promise((resolve, reject) => {
+    let isString = (typeof page === 'string')
+    let query = undefined
+    let adapter = {
+        items: [],
+        links: {},
+        meta: {}
+    }
 
-        isString 
-            ? query = _api + page 
-            : query = _api + _productParam + page 
-        axios.get(query)
-            .then(response => {
-                for (let key in adapter) adapter[key] = response.data[`_${key}`]
-                adapter.meta.last_page = Math.ceil(
-                    adapter.meta.total / adapter.meta.max_results
-                )
-                resolve(adapter)
-            })
-            .catch(error => {
-                console.warn(`Error in API ${error}`)
-                reject(error)
-            })
-    })
-}
+    isString
+        ? query = _api + page
+        : query = _api + _productParam + page
+    axios.get(query)
+        .then(response => {
+            for (let key in adapter) adapter[key] = response.data[`_${key}`]
+            adapter.meta.last_page = Math.ceil(
+                adapter.meta.total / adapter.meta.max_results
+            )
+            resolve(adapter)
+        })
+        .catch(error => {
+            console.warn(`Error in API ${error}`)
+            reject(error)
+        })
+})
