06d99cd3fbd8024e4124278d398f13a8b6344ba0
newGermes <kukhar.vitalii@gmail.com>
2018-01-09 18:02:40 +0200

    clear code

--- src/util/adapterForApiProducts.js
+++ src/util/adapterForApiProducts.js
@@ -4,30 +4,26 @@ export const adapter = response => {
         links: {},
         meta: {}
     }
-    let temp_items = []
 
     for (let key in adapter) adapter[key] = response.data[`_${key}`]
-    for (let item of adapter.items) {
-        temp_items.push({
-            id: item.id,
-            price: item.price ? item.price.retail : 0,
-            name: item.name ? item.name : 'no name',
-            img_url: item.images[0]
-                        ? item.images[0].thumbnails.category.url
-                        : 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg',
-            subject: item.meta["Тематика"]
-                            ? item.meta["Тематика"][0]
-                            : 'no subject',
-            quantity: item.meta["Количество деталей"]
-                            ? item.meta["Количество деталей"][0]
-                            : 'no quantity',
-            size: item.meta["Размер пазла"]
-                        ? item.meta["Размер пазла"][0]
-                        : 'no size',
-            age: item.meta["Возраст"] ? item.meta["Возраст"][0] : 'no age'
-        })
-    }
-    adapter.items = temp_items
+    adapter.items = adapter.items.map(item => ({
+      id: item.id,
+      price: item.price ? item.price.retail : 0,
+      name: item.name ? item.name : 'no name',
+      img_url: item.images[0]
+                  ? item.images[0].thumbnails.category.url
+                  : 'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg',
+      subject: item.meta["Тематика"]
+                      ? item.meta["Тематика"][0]
+                      : 'no subject',
+      quantity: item.meta["Количество деталей"]
+                      ? item.meta["Количество деталей"][0]
+                      : 'no quantity',
+      size: item.meta["Размер пазла"]
+                  ? item.meta["Размер пазла"][0]
+                  : 'no size',
+      age: item.meta["Возраст"] ? item.meta["Возраст"][0] : 'no age'
+    }))
 
     /**
      * add a number of the last page
--- src/util/numericPagination.js
+++ src/util/numericPagination.js
@@ -1,41 +1,38 @@
 export const getNumerical = (index, length, activeNumericPages) => {
-    let temp_array = []
-    let template_array = [index - 4 > 0 ? index - 4 : 0, 
-                    index - 3 > 0 ? index - 3 : 0, 
-                    index - 2 > 0 ? index - 2 : 0, 
-                    index - 1 > 0 ? index - 1 : 0, 
-                    index, 
+    let statePages = [index - 4 > 0 ? index - 4 : 0,
+                    index - 3 > 0 ? index - 3 : 0,
+                    index - 2 > 0 ? index - 2 : 0,
+                    index - 1 > 0 ? index - 1 : 0,
+                    index,
                     index + 1 > length ? 0 : index + 1,
-                    index + 2 > length ? 0 : index + 2, 
-                    index + 3 > length ? 0 : index + 3, 
-                    index + 4 > length ? 0 : index + 4].filter(elm => elm > 0)
-                   
-    template_array.forEach((elm, i) => {
-        elm === index
-        ? temp_array.push({ index: elm, view: elm, active: true })
-        : temp_array.push({ index: elm, view: elm, active: false })
-    })
-
-    if (temp_array[0].index !== 1) {
-        temp_array[0].index = temp_array[0].view = 1
-        temp_array[1].index = temp_array[2].index - 1
-        temp_array[1].view = '...'
+                    index + 2 > length ? 0 : index + 2,
+                    index + 3 > length ? 0 : index + 3,
+                    index + 4 > length ? 0 : index + 4]
+          .filter(elm => elm > 0)
+          .map(elm => (elm === index)
+                        ? { index: elm, view: elm, active: true }
+                        : { index: elm, view: elm, active: false })
+// rewrite -- start
+    if (statePages[0].index !== 1) {
+        statePages[0].index = statePages[0].view = 1
+        statePages[1].index = statePages[2].index - 1
+        statePages[1].view = '...'
     }
-    if (temp_array[temp_array.length - 1].index !== length) {
-        temp_array[temp_array.length - 1].index 
-            = temp_array[temp_array.length - 1].view = length
-        temp_array[temp_array.length - 2].index = 
-            temp_array[temp_array.length - 1].index - 1
-        temp_array[temp_array.length - 2].view = '...'
+    if (statePages[statePages.length - 1].index !== length) {
+        statePages[statePages.length - 1].index
+            = statePages[statePages.length - 1].view = length
+        statePages[statePages.length - 2].index =
+            statePages[statePages.length - 1].index - 1
+        statePages[statePages.length - 2].view = '...'
     }
-
-    activeNumericPages.length 
+// rewtire end
+    activeNumericPages.length
         ? activeNumericPages.forEach(page => {
-            temp_array.forEach(item => {
+            statePages.forEach(item => {
                 item.index === page ? item.active = true : false
-            })            
+            })
           })
         : activeNumericPages[0] = index
 
-    return temp_array
+    return statePages
 }
