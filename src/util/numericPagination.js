18cbf458d9cb03972f38ac491075f380a50f0c0c
newGermes <kukhar.vitalii@gmail.com>
2018-01-11 19:01:54 +0200

    start rewriting numeric Pagination

--- src/util/numericPagination.js
+++ src/util/numericPagination.js
@@ -1,5 +1,5 @@
 export const getNumerical = (index, length, activeNumericPages) => {
-  let statePages = [index - 4 > 0 ? index - 4 : 0,
+  const stateIndexes = [index - 4 > 0 ? index - 4 : 0,
                   index - 3 > 0 ? index - 3 : 0,
                   index - 2 > 0 ? index - 2 : 0,
                   index - 1 > 0 ? index - 1 : 0,
@@ -12,29 +12,38 @@ export const getNumerical = (index, length, activeNumericPages) => {
                     .map(elm => (elm === index)
                                   ? { index: elm, view: elm, active: true }
                                   : { index: elm, view: elm, active: false })
-// rewrite -- start
-  if (statePages[0].index !== 1) {
-    statePages[0].index = statePages[0].view = 1
-    statePages[1].index = statePages[2].index - 1
-    statePages[1].view = '...'
-  } else {
 
+  const addDots = array => {
+    let elm1 = array[0]
+    let elm2 = array[1]
+    let elm3 = array[2]
+    let elmLast = array[array.length - 1]
+    let elmPrevLast = array[array.length - 2]
+
+    // if (elm1.index !== 1 && )
   }
-  if (statePages[statePages.length - 1].index !== length) {
-    statePages[statePages.length - 1].index
-        = statePages[statePages.length - 1].view = length
-    statePages[statePages.length - 2].index =
-        statePages[statePages.length - 1].index - 1
-    statePages[statePages.length - 2].view = '...'
+
+// rewrite -- start
+  if (stateIndexes[0].index !== 1) {
+    stateIndexes[0].index = stateIndexes[0].view = 1
+    stateIndexes[1].index = stateIndexes[2].index - 1
+    stateIndexes[1].view = '...'
+  }
+  if (stateIndexes[stateIndexes.length - 1].index !== length) {
+    stateIndexes[stateIndexes.length - 1].index
+      = stateIndexes[stateIndexes.length - 1].view = length
+    stateIndexes[stateIndexes.length - 2].index =
+      stateIndexes[stateIndexes.length - 1].index - 1
+    stateIndexes[stateIndexes.length - 2].view = '...'
   }
 // rewtire -- end
   activeNumericPages.length
     ? activeNumericPages.forEach(page => {
-        statePages.forEach(item => {
-            item.index === page ? item.active = true : false
+        stateIndexes.forEach(item => {
+          item.index === page ? item.active = true : false
         })
       })
     : activeNumericPages[0] = index
 
-  return statePages
+  return stateIndexes
 }
